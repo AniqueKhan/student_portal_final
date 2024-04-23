a0c474be2e29c66da525d5aef907174567e08843
AniqueKhanABC <anique.khan@alphabetacore.com>
2024-04-23 13:46:29 +0500

    modified allowed hosts

--- student_portal/student_portal/settings.py
+++ student_portal/student_portal/settings.py
@@ -11,7 +11,7 @@ ON_VPS=env("ON_VPS")=="true"
 SECRET_KEY = env("SECRET_KEY")
 
 if ON_VPS:
-    ALLOWED_HOSTS = ['184.94.215.214']
+    ALLOWED_HOSTS = ['184.94.215.214',"studentportal.pynabte.com",'www.studentportal.pynabte.com']
 else:
     ALLOWED_HOSTS = ['localhost']
 
