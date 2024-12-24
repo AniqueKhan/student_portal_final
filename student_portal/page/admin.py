85db0da5c8f38ea953ccaea65eae0e13f6372b3b
Anique Khan <anique.khan@toko.pk>
2024-12-24 14:28:38 +0500

    updated code

--- student_portal/page/admin.py
+++ student_portal/page/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Page
+from .models import Page,PostFileContent
 # Register your models here.
 admin.site.register(Page)
+admin.site.register(PostFileContent)
