e0858e7e598d9e233afda6fb87638bc7335f1052
Anique Khan <anique.khan@toko.pk>
2025-01-02 11:10:49 +0500

    updated code

--- student_portal/module/models.py
+++ student_portal/module/models.py
@@ -9,9 +9,9 @@ class Module(models.Model):
     title = models.CharField(max_length=150,unique=True)
     description = models.TextField(null=True,blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="module_user")
-    pages = models.ManyToManyField(Page)
-    quizzes = models.ManyToManyField(Quiz)
-    assignments = models.ManyToManyField(Assignment)
+    pages = models.ManyToManyField(Page,blank=True)
+    quizzes = models.ManyToManyField(Quiz,blank=True)
+    assignments = models.ManyToManyField(Assignment,blank=True)
 
 
     def __str__(self):
