07d3149487063a4c940dee6b22c0b32febe4aa1e
Anique Khan <anique.khan@toko.pk>
2025-01-02 10:59:30 +0500

    updated description code

--- student_portal/classroom/models.py
+++ student_portal/classroom/models.py
@@ -67,7 +67,7 @@ class Course(models.Model):
         return self.title
 
     def get_short_description(self):
-        return self.description[:100] if len(self.description) > 100 else self.description
+        return self.description[:100] + "..." if len(self.description) > 100 else self.description
 
 class Grade(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
